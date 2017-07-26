65ce12ca4309ab98e73aa798c1d3e2f81bd44b07
compostbrain <joel@wildmeadowsfarm.com>
2017-07-26 11:29:24 -0400

    updated routes so devise is first

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   resources :wikis
   resources :users
 
-  devise_for :users
+
 
   get 'about' => 'welcome#about'
 
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get 'faq' => 'welcome#faq'
 
-  root  'welcome#index'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
